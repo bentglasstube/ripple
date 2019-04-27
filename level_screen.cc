781f9d7e31d6e83018ca391cba5dbab9b713cb95
Alan Berndt <alan@eatabrick.org>
2019-04-27 11:35:39 -0700

    control only one player at a time

--- level_screen.cc
+++ level_screen.cc
@@ -6,20 +6,23 @@ LevelScreen::LevelScreen(GameState state) :
   map_(), p1_(false), p2_(true) {}
 
 bool LevelScreen::update(const Input& input, Audio& audio, unsigned int elapsed) {
+  if (input.key_pressed(Input::Button::Select)) {
+    control_inverted_ = !control_inverted_;
+  }
+
+  Player& p = control_inverted_ ? p2_ : p1_;
+
+  p1_.stop_moving();
+  p2_.stop_moving();
+
   if (input.key_held(Input::Button::Left)) {
-    p1_.move_left();
-    p2_.move_left();
+    p.move_left();
   } else if (input.key_held(Input::Button::Right)) {
-    p1_.move_right();
-    p2_.move_right();
-  } else {
-    p1_.stop_moving();
-    p2_.stop_moving();
+    p.move_right();
   }
 
   if (input.key_pressed(Input::Button::A)) {
-    p1_.jump();
-    p2_.jump();
+    p.jump();
   }
 
   p1_.update(map_, elapsed);
